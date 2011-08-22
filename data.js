5491d314471cce0b379d786580c2a78be44603c5
Colin Snover <github.com@zetafleet.com>
2011-08-22 20:05:33 +0000

    Simplify step 3 code.

--- data.js
+++ data.js
@@ -1,15 +1,17 @@
 // quick and dirty global catalog data
-junkCatalogData = [
-	{ name: "Wrist watch", image: "watch.jpg", description: "Tell time with Swiss precision", quantity: 3 },
-	{ name: "Life jacket", image: "life-jacket.jpg", description: "Stay afloat during your frequent shipwrecks", quantity: 1 },
-	{ name: "Toy bulldozer", image: "bulldozer.jpg", description: "Move the earth, one cup at a time", quantity: 8 },
-	{ name: "Vintage microphone", image: "microphone.jpg", description: "Make 'em swoon in your blue suede shoes", quantity: 0 },
-	{ name: "TIE fighter", image: "tie-fighter.jpg", description: "Rid your neighborhood of pesky Rebel Scum", quantity: 0 }
-];
-
-foodCatalogData = [
-	{ name: "Apples", image: "apple.jpg", description: "Keep the doctor away on a daily basis", quantity: 10 },
-	{ name: "Bananas", image: "bananas.jpg", description: "Bananas are an excellent source of potassium", quantity: 18 },
-	{ name: "Tomatoes", image: "tomatoes.jpg", description: "Imported fresh daily from Dylan's front porch", quantity: 0 },
-	{ name: "Bread", image: "bread.jpg", description: "Guaranteed to be either stale or fresh", quantity: 7 }
-];
+define({
+	junkCatalogData: [
+		{ name: "Wrist watch", image: "watch.jpg", description: "Tell time with Swiss precision", quantity: 3 },
+		{ name: "Life jacket", image: "life-jacket.jpg", description: "Stay afloat during your frequent shipwrecks", quantity: 1 },
+		{ name: "Toy bulldozer", image: "bulldozer.jpg", description: "Move the earth, one cup at a time", quantity: 8 },
+		{ name: "Vintage microphone", image: "microphone.jpg", description: "Make 'em swoon in your blue suede shoes", quantity: 0 },
+		{ name: "TIE fighter", image: "tie-fighter.jpg", description: "Rid your neighborhood of pesky Rebel Scum", quantity: 0 }
+	],
+	
+	foodCatalogData: [
+		{ name: "Apples", image: "apple.jpg", description: "Keep the doctor away on a daily basis", quantity: 10 },
+		{ name: "Bananas", image: "bananas.jpg", description: "Bananas are an excellent source of potassium", quantity: 18 },
+		{ name: "Tomatoes", image: "tomatoes.jpg", description: "Imported fresh daily from Dylan's front porch", quantity: 0 },
+		{ name: "Bread", image: "bread.jpg", description: "Guaranteed to be either stale or fresh", quantity: 7 }
+	]
+});
--- util.js
+++ util.js
@@ -1,47 +1,16 @@
-define([ "dojo/dom-class", "dojo/dnd/Source" ], function(domClass, Source){
+define([ "dojo/string", "dojo/dom-construct", "dojo/dom-class", "dojo/dnd/Source", "dojo/text!./itemTemplate.html", "dojo/text!./avatarTemplate.html" ], function(stringUtil, domConstruct, domClass, Source, template, avatarTemplate){
 	// create the DOM representation for the given item
 	function catalogNodeCreator(item, hint){
-		// create a table/tr/td-based node structure; each item here needs an
-		// image, a name, a brief description, and a quantity available
-		var tr = document.createElement("tr"),
-			imgTd = document.createElement("td"),
-			nameTd = document.createElement("td"),
-			qtyTd = document.createElement("td"),
-			img = document.createElement("img");
+		var node = domConstruct.toDom(stringUtil.substitute(hint === "avatar" ? avatarTemplate : template, {
+			name: item.name || "Product",
+			imageUrl: "images/" + (item.image || "_blank.gif"),
+			quantity: item.quantity || 0,
+			description: item.description ? "<br><span>" + item.description + "</span>" : ""
+		})),
 		
-		img.src = "images/" + (item.image || "_blank.gif");
-		domClass.add(imgTd, "itemImg dojoDndHandle");
-		imgTd.appendChild(img);
+		type = item.quantity ? [ "inStock" ] : [ "outOfStock" ];
 
-		nameTd.appendChild(document.createTextNode(item.name || "Product"));
-		if(item.description && hint != "avatar"){
-			var descSpan = document.createElement("span");
-			descSpan.innerHTML = item.description;
-			nameTd.appendChild(document.createElement("br"));
-			nameTd.appendChild(descSpan);
-		}
-		
-		domClass.add(nameTd, "itemText");
-
-		tr.appendChild(imgTd);
-		tr.appendChild(nameTd);
-
-		if(hint != "avatar"){
-			qtyTd.innerHTML = item.quantity;
-			domClass.add(qtyTd, "itemQty");
-			tr.appendChild(qtyTd);
-		}else{
-			// put the avatar into a self-contained table
-			var table = document.createElement("table"),
-				tbody = document.createElement("tbody");
-			tbody.appendChild(tr);
-			table.appendChild(tbody);
-			node = table;
-		}
-
-		var type = item.quantity ? [ "inStock" ] : [ "outOfStock" ];
-
-		return { node: tr, data: item, type: type };
+		return { node: node, data: item, type: type };
 	}
 
 	// create a dojo.dnd.Source from the data provided
